c38fbbc24343ff6630e97684e736ff9a85739fcc
IvanShterev <122817920+IvanShterev@users.noreply.github.com>
2023-01-17 11:03:37 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -8,5 +8,10 @@ keys.addEventListener('click', event => {
     const key = event.target;
     const keyValue = key.textContent;
     const displayValue = display.textContent;
-    display.textContent = keyValue;
+    if(displayValue === '0'){
+        display.textContent = keyValue;
+    }
+    else{
+        display.textContent = displayValue + keyValue;
+    }
 })
